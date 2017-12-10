a20a52991edf31e2b425cadfb0c855d5b68e7de8
赛冷思 <silence@sailengsi.com>
2017-12-10 20:56:24 +0800

    test

--- cooking.conf.js
+++ cooking.conf.js
@@ -2,35 +2,36 @@ var path = require('path');
 var cooking = require('cooking');
 
 cooking.set({
-  entry: {
-    app: ['babel-polyfill', './src/main.js']
-  },
-  dist: './dist',
-  template: './index.tpl',
+    entry: {
+        app: ['babel-polyfill', './src/main.js']
+    },
+    dist: './dist',
+    template: './index.tpl',
 
-  devServer: {
-    port: 8080,
-    publicPath: '/'
-  },
+    devServer: {
+        port: 8881,
+        publicPath: '/'
+    },
 
-  // production
-  clean: true,
-  hash: true,
-  sourceMap: true,
-  minimize: true,
-  chunk: true, // see https://cookingjs.github.io/zh-cn/configuration.html#chunk
-  postcss: [
-    // require('...')
-  ],
-  publicPath: '/dist/',
-  assetsPath: 'static',
-  urlLoaderLimit: 10000,
-  static: true,
-  extractCSS: '[name].[contenthash:7].css',
-  alias: {
-    'src': path.join(__dirname, 'src')
-  },
-  extends: ['vue2', 'lint', 'less', 'autoprefixer']
+    // production
+    clean: true,
+    hash: true,
+    sourceMap: true,
+    minimize: true,
+    chunk: true, // see https://cookingjs.github.io/zh-cn/configuration.html#chunk
+    postcss: [
+        // require('...')
+    ],
+    publicPath: '/dist/',
+    assetsPath: 'static',
+    urlLoaderLimit: 10000,
+    static: true,
+    extractCSS: '[name].[contenthash:7].css',
+    alias: {
+        'src': path.join(__dirname, 'src')
+    },
+    // 'lint',
+    extends: ['vue2', 'less', 'autoprefixer']
 });
 
 module.exports = cooking.resolve();
